6798d6ca915b45a105f160f8c064ed03ec17f41c
Yogaraj.S <78194098+yogarajjuju@users.noreply.github.com>
2026-03-21 08:59:28 +0000

    n

--- Week1/Matrix.java
+++ Week1/Matrix.java
@@ -5,8 +5,9 @@ class Matrix{
     {4, 5, 6},
     {7, 8, 9}
 };
-int sum=0;
-        for(int i = 0; i < matrix.length; i++){        // rows
+
+        for(int i = 0; i < matrix.length; i++){
+            int sum=0;        // rows
     for(int j = 0; j < matrix[0].length; j++){  // columns
        sum+=matrix[i][j];
     }
